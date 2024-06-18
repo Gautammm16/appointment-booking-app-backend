292a086d43a4c9a62365c5391d6b2ba291bf1ea4
Gautam <gautamnjaisur@gmail.com>
2024-06-18 17:32:04 +0530

    commited

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,30 @@
-import jwt from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
 
-module.exports = (req,res,next)=>{
-    
-    
-}
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"]?.split(" ")[1];
+    if (!token) {
+      return res.status(401).send({
+        message: "Auth Failed",
+        success: false,
+      });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({
+          message: "Auth Failed",
+          success: false,
+        });
+      } else {
+        req.body.userId = decoded.id;
+        next();
+      }
+    });
+  } catch (e) {
+    return res.status(401).send({
+      message: "Auth Failed",
+      success: false,
+    });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,68 +3,71 @@ const router = express.Router();
 const User = require("../models/user.model.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/auth.middleware.js");
 
 router.post("/register", async (req, res) => {
   try {
     const userExist = await User.findOne({ email: req.body.email });
 
     if (userExist) {
-      return res
-        .status(200)
-        .send({ message: "User Already Exist", success: false });
+      return res.status(200).send({ message: "User Already Exists", success: false });
     }
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({ ...req.body, password: hashedPassword });
-    // const newUser = new User(req.body);
     await newUser.save();
-    res
-      .status(200)
-      .send({ message: "User created sucessfully", success: true });
+    res.status(200).send({ message: "User created successfully", success: true });
   } catch (e) {
-    console.log(e)
-    res.status(500).send({ message: "Error creating user", sucess: false });
+    console.log(e);
+    res.status(500).send({ message: "Error creating user", success: false });
   }
 });
-
 
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
-      return res
-        .status(200)
-        .send({ message: "User does not exist", success: false });
+      return res.status(200).send({ message: "User does not exist", success: false });
     }
 
     const isMatch = await bcrypt.compare(req.body.password, user.password);
 
     if (!isMatch) {
-      return res
-        .status(200)
-        .send({ message: "Incorrect password", success: false });
+      return res.status(200).send({ message: "Incorrect password", success: false });
     } else {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "1d", 
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { //id underscore
+        expiresIn: "1d",
       });
-      res
-      .status(200)
-      .send({ message: "Login successful", success: true, data: token });
+      res.status(200).send({ message: "Login successful", success: true, data: token });
     }
-   } catch (e) {
+  } catch (e) {
     console.log(e);
-    res.status(500).send({ message: "Error logging in", success: false ,e});
+    res.status(500).send({ message: "Error logging in", success: false, error: e });
   }
 });
 
 
-router.post('get-user-by-id',async(token)=>{
+
+router.get("/get-user-info-by-id", authMiddleware, async (req, res) => {
   try {
-    
+    const userId = req.query.userId;
+    const user = await User.findOne({ id: userId }); // id underscore
+    if (!user) {
+      return res.status(200).send({ message: "User does not exist", success: false });
+    } else {
+      res.status(200).send({
+        success: true,
+        data: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+    }
   } catch (error) {
-    
+    res.status(500).send({ message: "Error getting user info", success: false, error });
   }
-})
+});
+
 module.exports = router;
